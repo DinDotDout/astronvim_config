7c7fc3b9dcd5160ba96a7e12bec3de0775065a71
Joan Dot Sastre <60853685+DinDotDout@users.noreply.github.com>
2023-06-13 20:27:52 +0200

    Update user.lua
    test

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,15 @@
 return {
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "User AstroFile",
+    cmd = {"TodoQuickFix"},
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    }
+  },
+  require'lspconfig'.gdscript.setup{},
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
